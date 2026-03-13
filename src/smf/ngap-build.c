c105d5800000e222d34154e2fe778f72bd54d76e
umroot <umroot@shadowbyte.encs.concordia.ca>
2026-03-13 12:44:37 -0400

    version1

--- src/smf/ngap-build.c
+++ src/smf/ngap-build.c
@@ -56,13 +56,15 @@ static void fill_qos_level_parameters(
     /* Optional GBR/MBR Information */
     if (include_gbr &&
         qos->mbr.downlink && qos->mbr.uplink &&
-        qos->gbr.downlink && qos->gbr.uplink, bool qnc ) { // kassem qnc
+        qos->gbr.downlink && qos->gbr.uplink) {
         NGAP_GBR_QosInformation_t *gBR_QosInformation =
             params->gBR_QosInformation = CALLOC(1, sizeof(*gBR_QosInformation));
         ogs_assert(gBR_QosInformation);
-        if (qnc) {
-            gBR_QosInformation->notificationControl = CALLOC(1, sizeof(long));
-            *gBR_QosInformation->notificationControl = NGAP_NotificationControl_requested;
+        if (qnc) { //kassem
+             gBR_QosInformation->notificationControl =
+                CALLOC(1, sizeof(long));
+            ogs_assert(gBR_QosInformation->notificationControl);
+            *gBR_QosInformation->notificationControl = 0; /* requested */
         }  
         ogs_assert(qos->mbr.downlink <= OGS_MAX_BITRATE_NGAP);
         ogs_assert(qos->mbr.uplink <= OGS_MAX_BITRATE_NGAP);
@@ -482,7 +484,7 @@ ogs_pkbuf_t *ngap_build_pdu_session_resource_modify_request_transfer(
 
                     fill_qos_level_parameters(
                             QosFlowAddOrModifyRequestItem->
-                                qosFlowLevelQosParameters, &qos, false); //kassem false
+                                qosFlowLevelQosParameters, &qos, true); //kassem false
                 }
             }
         }
